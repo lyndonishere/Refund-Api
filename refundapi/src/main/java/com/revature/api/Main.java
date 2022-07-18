1924107299a346829995be58e40e5bfb239c49f4
odanby <oriana.danby@gmail.com>
2022-07-18 11:30:08 -0700

    fixed null pointers

--- refundapi/src/main/java/com/revature/api/Main.java
+++ refundapi/src/main/java/com/revature/api/Main.java
@@ -1,6 +1,11 @@
 package com.revature.api;
 
 import com.revature.controllers.LoginController;
+import com.revature.repository.LoginDAO;
+import com.revature.repository.LoginDAOInterface;
+import com.revature.service.LoginService;
+import com.revature.service.LoginServiceInterface;
+import com.revature.utils.LoginBusinessRules;
 
 import io.javalin.Javalin;
 
@@ -15,9 +20,18 @@ public class Main {
             config.enableDevLogging();
         });  
 
-        LoginController loginController = new LoginController();
+        /* 
+         * For our controller class to be able to send info to the service layer for validation it must be given a service object. However, that service object requires
+         * its own dependencies to work correctly, so this is where we create our official objects for our repo and service layers so that our API can correctly
+         * send information where it needs to go
+         */
+        LoginDAOInterface loginDao = new LoginDAO();
+        LoginBusinessRules loginBusinessRules = new LoginBusinessRules();
+        LoginServiceInterface loginService = new LoginService(loginDao, loginBusinessRules);
+        LoginController loginController = new LoginController(loginService);
         
         app.get("/hello", loginController.getHelloWorld);
+        app.get("/getAllUsers", loginController.getAllUsers);
 
         // app.post();
 
--- refundapi/src/main/java/com/revature/controllers/LoginController.java
+++ refundapi/src/main/java/com/revature/controllers/LoginController.java
@@ -1,9 +1,21 @@
 package com.revature.controllers;
 
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.revature.entities.Login;
+import com.revature.service.LoginServiceInterface;
+
 import io.javalin.http.Handler;
 
 public class LoginController {
    
+   private LoginServiceInterface loginService;
+
+   public LoginController(LoginServiceInterface loginService){
+      this.loginService = loginService;
+   }
+
     /*
      * This LoginController is going to parse the info received in HTTP request bodies, send the information to the appropriate service method, and craft an HTTP
      * response for us based upon the data received from the service methods
@@ -16,5 +28,20 @@ public class LoginController {
      public Handler getHelloWorld = ctx -> {
         ctx.result("Hello world!");
         ctx.status(200);
+     };
+
+     // API -> service -> repo -> service -> API
+     public Handler getAllUsers = ctx -> {
+      List<Login> login =  this.loginService.serviceGetAllUsers();
+      /*
+       * The login arrayList above contains multiple Java login objects: these need to turn into JSON formatting to be able to attach them to the HTTP Response Body
+       * We can use the GSON library we acquired from the Maven repository to handle this conversion process for us
+       */
+      Gson gson = new Gson();
+      // the toJson metod provided by Gson turns our collection of Book objects into a formatted string
+      String loginJSON = gson.toJson(login);
+      // we use the ctx.result() method to attack the loginJSON to the response body
+      ctx.result(loginJSON);
+      ctx.status(200);
      };
 }
