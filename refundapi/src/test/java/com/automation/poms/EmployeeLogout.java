ac6a4b425f15f7f11c28d98898004d5cac1c6cb4
MauriceKK <maurice.kabeireho@gmail.com>
2022-07-27 15:26:54 -0400

    updated the Employee login/logout

--- refundapi/src/test/java/com/automation/poms/EmployeeLogout.java
+++ refundapi/src/test/java/com/automation/poms/EmployeeLogout.java
@@ -1,12 +1,40 @@
 package com.automation.poms;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class EmployeeLogout {
+    private WebDriver driver;
 
     public EmployeeLogout(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
+
+    @FindBy(id ="logout-button")
+    public WebElement employeeLogoutButton;
+    public void employeeClickLogoutButton(){
+        this.employeeLogoutButton.click();
+
+    }
+
+    public void employeeConfirmLogout(){
+        WebDriverWait waitEmployeeLogout = new WebDriverWait(driver,10);
+        waitEmployeeLogout.until(ExpectedConditions.alertIsPresent());
+        Alert alertEmployeeLogout = driver.switchTo().alert();
+        alertEmployeeLogout.accept();
+    }
+
+    public void noLogout(){
+        WebDriverWait waitEmployeeLogoutNow = new WebDriverWait(driver,10);
+        waitEmployeeLogoutNow.until(ExpectedConditions.alertIsPresent());
+        Alert alertEmployeeLogoutNow = driver.switchTo().alert();
+        alertEmployeeLogoutNow.dismiss();
+        
+
     
 }
--- refundapi/src/test/java/com/automation/steps/EmployeeLogoutSteps.java
+++ refundapi/src/test/java/com/automation/steps/EmployeeLogoutSteps.java
@@ -8,32 +8,29 @@ import io.cucumber.java.en.When;
 
 public class EmployeeLogoutSteps {
     
-    // scenarios 1 and 2
+    
     @Given("the employee is on their personal employee homepage")
         public void the_employee_is_on_their_personal_employee_homepage(){
             TestRunner.driver.get("File:///Users/mauricekabeireho/_dev/Refund-Api/refundapi/src/test/resources/web-pages/EmployeePage.html");
         }
 
-    // scenarios 1 and 2
+    // scenarios 1 
     @When("the employee clicks the logout button")
         public void the_employee_clicks_the_logout_button(){
+            TestRunner.employeeLogout.employeeClickLogoutButton();
         }
 
     // scenario 1
     @When("the employee confirms they want to logout")
         public void the_employee_confirms_they_want_to_logout(){
+            TestRunner.employeeLogout.employeeConfirmLogout();
         }
 
     @Then("the employee should be logged out and returned back to the login homepage")
         public void the_employee_should_be_logged_out_and_returned_back_to_the_login_homepage(){
-        }
+            TestRunner.driver.get("File:///Users/mauricekabeireho/_dev/Refund-Api/refundapi/src/test/resources/web-pages/LoginPage.html");
+       
+    }
 
-    // scenario 2
-    @When("the employee does not confirm they want to logout")
-        public void the_employee_does_not_confirm_they_want_to_logout(){ 
-        }
-        
-    @Then("the employee should stay on their current page")
-        public void the_employee_should_stay_on_their_current_page(){
-        }
+
 }
